6597b33409a0669fdf3fcfd851f69a6c06274973
lpfrontendvn <lpfrontend.vn@gmail.com>
2021-06-09 14:00:10 +0300

    Add more media

--- Edu/#src/js/script.js
+++ Edu/#src/js/script.js
@@ -9,8 +9,9 @@ $(function(){
         arrows: true,
         dots: false,
         centerMode: true,
-        slidesToShow: 1
-        
+        slidesToShow: 1,
+        nextArrow: '<img src="../img/arrright.png" alt="" class="next">',
+        prevArrow: '<img src="../img/arrleft.png" alt="" class="prev">',
     });
         $('.menu_btn').on('click', function(){
             $('.menu_list').toggleClass('menu_list--active');
