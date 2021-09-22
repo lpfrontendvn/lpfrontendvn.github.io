fc6f67b090760e126080cc5222173defff482124
Lubov Popova <lpfrontend.vn@gmail.com>
2021-09-22 10:38:02 +0300

    add xor

---
+++ xor/js/script.js
@@ -0,0 +1,15 @@
+$(function() {
+    $('.menu_btn').on('click', function(){
+        $('.menu_list').toggleClass('menu_list--active');
+    });
+});
+
+// $(document).ready(function(event) {
+//     $('.menu_btn').click(function(event){
+//         $('.menu_btn, .menu_list').toggleClass('menu_list--active');
+//     });
+// });
+
+
+
+  
