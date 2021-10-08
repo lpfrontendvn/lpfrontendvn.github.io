73912fd6d8d3279c3b22ffdd1edcc4de7cf36500
Lubov Popova <lpfrontend.vn@gmail.com>
2021-10-08 18:21:49 +0300

    add traveland

---
+++ trevland/js/script.js
@@ -0,0 +1,50 @@
+$(function() {
+    $('.menu_btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+    });
+});
+$(function(){
+    $('.offers__slider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        variableWidth: true,
+        
+        responsive: [
+            {
+              breakpoint: 1107,
+              settings: {
+                slidesToShow: 4,
+              }
+            },
+              {
+                breakpoint: 566,
+                settings: {
+                  slidesToShow: 2,
+                  centerMode: true,
+                  swipe: true,
+                }
+              },
+              {
+                breakpoint: 460,
+                settings: {
+                  slidesToShow: 1,  
+                }
+              },
+              {
+                breakpoint: 425,
+                settings: {
+                  slidesToShow: 1,
+                  variableWidth: false,
+                  
+                }
+              },
+        ]
+    });
+
+  });
+  
+
+
+  
